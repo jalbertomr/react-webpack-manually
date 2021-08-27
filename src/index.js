f1c96f917ecc6b9891c5e7024194a6825fefec09
jalbertomr <jalbertomr@yahoo.com>
2021-08-27 00:40:28 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Test React Webpack Babel configuration.</h1>, 
+                document.getElementById('root'));
+
+
+console.log('Config Webpack OK!');
