bdcf70b71def95ec07be66868a267b3e46dacb43
jalbertomr <jalbertomr@yahoo.com>
2021-08-30 15:30:08 -0500

    modify to load scss and test it

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/styles.css';
+import './css/styles.scss';
 
-ReactDOM.render(<h1>Test React Webpack Babel css configuration.</h1>, 
+ReactDOM.render(<h1>Test React Webpack Babel scss configuration.</h1>, 
                 document.getElementById('root'));
 
 
