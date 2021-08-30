1fcb3a37a20b5cbae480387efc725713a66664a4
jalbertomr <jalbertomr@yahoo.com>
2021-08-30 15:13:36 -0500

    add css-loader,style-loader and test it

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/styles.css';
 
-ReactDOM.render(<h1>Test React Webpack Babel configuration.</h1>, 
+ReactDOM.render(<h1>Test React Webpack Babel css configuration.</h1>, 
                 document.getElementById('root'));
 
 
